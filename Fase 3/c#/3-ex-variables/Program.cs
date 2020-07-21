0489f73eca9dc1c979a6b32e258ba149715f5b7a
karina-89 <karina.ramirez.kr89@gmail.com>
2020-07-21 21:51:59 +0200

    fase 3

---
+++ Fase 3/c#/3-ex-variables/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _3_ex_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano_bisiesto = 1948;
+            int ano_nacimiento = 1984;
+            int cada_cuanto = 4;
+            bool nacimiento_bisiesto = false;
+            string nombre = "Karina Rodriguez Ramirez";
+            string fecha_nacimiento = "25/05/1984";
+
+            for (int i = ano_bisiesto; i <= ano_nacimiento; i+=cada_cuanto)
+            {
+                Console.WriteLine("Año bisiesto: " + i);
+                if (i == ano_nacimiento)
+                    nacimiento_bisiesto = true;
+            }
+            
+            string nacimiento = "";
+            if (nacimiento_bisiesto == true)
+                nacimiento = "Mi año de nacimiento es bisiesto";
+            else
+                nacimiento = "Mi año de nacimiento no es bisiesto";
+
+            Console.WriteLine("Mi nombre es " + nombre);
+            Console.WriteLine("Nací el " + fecha_nacimiento);
+            Console.WriteLine(nacimiento);
+        }
+    }
+}
