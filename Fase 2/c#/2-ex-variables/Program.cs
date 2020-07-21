16857f253ae9710d92cce347a3f9981166ae8404
karina-89 <karina.ramirez.kr89@gmail.com>
2020-07-21 21:25:04 +0200

    fase 2

---
+++ Fase 2/c#/2-ex-variables/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_ex_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int ano_bisiesto = 1948;
+            int cada_cuanto = 4;
+            int resultado = (1984 - ano_bisiesto) / cada_cuanto;
+
+            Console.WriteLine("Entre el año 1984 y el año " + ano_bisiesto + " hay " + resultado + " años bisiestos.");
+        }
+    }
+}
