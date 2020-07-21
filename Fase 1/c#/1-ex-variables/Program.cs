e9bf792ceaeff9eae2dd2ed73cf306672a722941
karina-89 <karina.ramirez.kr89@gmail.com>
2020-07-21 21:09:32 +0200

    fase 1

---
+++ Fase 1/c#/1-ex-variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1_ex_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Karina";
+            string apellido1 = "Rodriguez";
+            string apellido2 = "Ramirez";
+
+            int dia = 25;
+            int mes = 5;
+            int ano = 1984;
+
+            Console.WriteLine(apellido1 + " " + apellido2 + ", " + nombre);
+            Console.WriteLine(dia + "/" + mes + "/" + ano);
+        }
+    }
+}
